cba08b760c21cfd9456cf2e69295cb77c6f78a6a
Romulo <miojoblack5@gmail.com>
2025-02-06 11:05:53 -0400

    VendedorDaoJDBC e DaoFactory

---
+++ src/model/dao/impl/VendedorDaoJDBC.java
@@ -0,0 +1,41 @@
+package model.dao.impl;
+
+import java.util.List;
+
+import model.dao.VendedorDao;
+import model.entities.Vendedor;
+
+public class VendedorDaoJDBC implements VendedorDao{
+
+	@Override
+	public void Creat(Vendedor obj) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Vendedor Read(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void Update(Vendedor obj) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void Delete(int id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<Vendedor> readAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
+}
