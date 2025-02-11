8258265c3d0d2f6f5fc179a355a338be2e9e06e6
Romulo <miojoblack5@gmail.com>
2025-02-11 16:30:10 -0400

    implementando update

--- src/application/Program.java
+++ src/application/Program.java
@@ -17,23 +17,30 @@ public class Program {
 		Vendedor vendedor = vendedorDao.Read(1);
 		System.out.println(vendedor);
 		
-		System.out.println("=== teste: procurar vendedor pro departamento ====");
+		System.out.println("\n=== teste: procurar vendedor por departamento ====");
 		Departamento departamento = new Departamento(2, null);
 		List<Vendedor> list = vendedorDao.findByDepartment(departamento);
 		for(Vendedor obj : list) {
 			System.out.println(obj);
 		}
 		
-		System.out.println("=== teste: procurar tudo ====");
+		System.out.println("\n=== teste: procurar tudo ====");
 		list = vendedorDao.readAll();
 		for(Vendedor obj : list) {
 			System.out.println(obj);
 		}
 		
-		System.out.println("=== teste: inserir ====");
+		System.out.println("\n=== teste: inserir ====");
 		Vendedor novoVendedor = new Vendedor(1, "Paula", "paula@gmail.com", new Date(), 4000.0, departamento);
 		vendedorDao.Creat(novoVendedor);
 		System.out.println("Inserted! new id = " + novoVendedor.getId());
+		
+		System.out.println("\n=== teste: atualizar ====");
+		vendedor = vendedorDao.Read(1);
+		vendedor.setName("Vinicius");
+		vendedor.setEmail("vinicius@gmial.com");
+		vendedorDao.Update(vendedor);
+		System.out.println("Update completed!");
 	}
 
 }
--- src/model/dao/impl/VendedorDaoJDBC.java
+++ src/model/dao/impl/VendedorDaoJDBC.java
@@ -89,7 +89,27 @@ public class VendedorDaoJDBC implements VendedorDao{
 	
 	@Override
 	public void Update(Vendedor obj) {
-		// TODO Auto-generated method stub
+		PreparedStatement st = null;
+		try {
+			st = conn.prepareStatement(
+					"UPDATE vendedor "
+					+ "SET Nome = ?, Email = ?, DataDeNascimento = ?, SalarioBase = ?, DepartamentoId = ? "
+					+ "WHERE Id = ?");
+			
+			st.setString(1, obj.getName());
+			st.setString(2, obj.getEmail());
+			st.setDate(3, new java.sql.Date(obj.getBirthDate().getTime()));
+			st.setDouble(4, obj.getBaseSalary());
+			st.setInt(5, obj.getDepartment().getId());
+			st.setInt(6, obj.getId());
+			
+			st.executeUpdate();
+			
+		}catch(SQLException e) {
+			throw new DbException(e.getMessage());
+		}finally {
+			DB.closeStatement(st);
+		}
 		
 	}
 
